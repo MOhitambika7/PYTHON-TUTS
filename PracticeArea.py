6083021c1015bdd00cd66376c9100db676e27fca
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-21 18:46:58 +0545

    First basic exercise to find the area of rectangle

---
+++ PracticeArea.py
@@ -0,0 +1,5 @@
+# Exercice 1:- Area calculation of rectangle.
+Length = float(input("Length of rectangle:- "))
+Width = float(input("Width of rectangle:- "))
+Area = Length * Width
+print(f"The are of rectangle is: {Area}.")
