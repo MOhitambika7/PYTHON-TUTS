2a335eb5662c82db9b29961bd952fe78985e7ed4
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-21 19:53:03 +0545

    Madlibs game in python

---
+++ Game.py
@@ -0,0 +1,12 @@
+#Madlibs game
+#Word game where you create the story
+#By filling with the blanks with the random words. 
+Adjective1 = input("Adjective1:-")
+Noun1 = input("Noun:-")
+Adjective2 = input("Adjective2:- ")
+Verb1 = input("Verb1:- ")
+print(f"There was a {Adjective1} unicorn")
+print(f"There was one more animal which was{Noun1}")
+print(f"They were {Adjective2} friends")
+print(f"They both {Verb1} with each other")
+print(f"They were also known for {Adjective2} for the life time")
