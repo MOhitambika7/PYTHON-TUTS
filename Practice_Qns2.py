a0ddcbe2a6f627e3c75878b7cf0356005b146718
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-21 18:55:15 +0545

    Shopping cart practice qns

---
+++ Practice_Qns2.py
@@ -0,0 +1,6 @@
+# Exercise 2:- Sopping cart program.
+item = input("What item would you want to buy?: ")
+price = float(input("What is the price for the item?: "))
+quantity = int(input("How many item would you like to buy?: "))
+Total = price * quantity
+print(f"The total cost for the item purchased is:- {Total} ")
