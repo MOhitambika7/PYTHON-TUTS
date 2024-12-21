03c7e57de7ef4425abf99f00f70e52d7e4694525
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-21 13:51:52 +0545

    Code for different data types

---
+++ Second.py
@@ -0,0 +1,27 @@
+#Strings.
+
+firstName = "Bro"
+print(firstName)
+print(f"hey! {firstName}")
+
+food = "mo:mo"
+print(f"Mine favrouite food is:- {food}")
+
+#Integers.
+No_of_students = 30
+age = 15
+quantity = 3
+print(f"Total number of the students in the class are:- {No_of_students}")
+print(f"The quantity of sugar you bought is:- {quantity} kg")
+print(f"The age of the person A is:- {age}")
+
+#floating points.
+GPA = 3.68
+print(f"You got {GPA} in exams")
+
+#Boolean.
+isStudent = True
+if isStudent:
+    print("He is a student")
+else:
+    print("He is not a student")    
