2426ff5e4125115b615ff497c3644a623473a1b5
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-21 20:15:21 +0545

    About arithmatics operations on python and some math functions.

---
+++ Arithmatic_Operations.py
@@ -0,0 +1,46 @@
+Num = 0
+Num += 5
+print(Num)
+Num -= 2
+print(Num)
+Num *= 3
+print(Num)
+Num /= 3
+print(Num)
+
+#using +=, -=, *=, /= are the augmented operations.
+#Now using some math functions below:-
+x = 5.66
+y = -4
+z = 2
+result = round(x) 
+print(result)
+
+value = abs(y)
+print(value)
+
+power = pow(z, 2)
+print(power)
+
+maximum = max(x, y, z)
+print(maximum)
+
+minimum = min(x, y, z)
+print(minimum)
+
+#some imp features and modules:-
+import math 
+print(math.pi)  
+print(math.e)
+
+A = 9
+sqrt = math.sqrt(A)
+print(sqrt)
+
+S = 9.5
+ceiling = math.ceil(S) # This ceilling function round off the number to the up.
+print(S)
+
+F = 9.6
+floor = math.floor(F) # This floor function will round off the numbr to the down.
+print(F)
