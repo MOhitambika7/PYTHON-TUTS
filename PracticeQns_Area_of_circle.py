e5fb3579360f2900fe260a3575619708f432ef97
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-22 07:42:26 +0545

    Code to find area of circle using math function

---
+++ PracticeQns_Area_of_circle.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input("Enter the radius of the circle:- "))
+area = math.pi * radius * radius
+print(f"The area of circle is:- {area} cm^2")
+print(f"The rounded area is:- {round(area, 2)}") # Rounds area upto two decimal points.
