805622f4d455a143f68cd887be6fb91e58e238c7
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-22 22:46:10 +0545

    Code for boolean data type

---
+++ Boolean.py
@@ -0,0 +1,7 @@
+question = input("Is it raining? :- (Y/N) ")
+if question == "Y":
+    print("I will call my parents to take me home because i forgot to briing umbrella. ")
+elif question == "N":
+    print("I can go home by walking with my friends.")
+else:
+    print(f"Incorrect input {question}. Please give a corect input.")        
