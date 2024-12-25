869da4107347495346ecb4832c338be5e724fca7
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-25 19:40:44 +0545

    This is the code for the conditional statements using the concept of ternary operators.

---
+++ Conditional_Expressions.py
@@ -0,0 +1,13 @@
+# Conditional expressions = A one-line shortcut  for the if-else statement (ternary operator).
+
+Num1 = float(input("Enter the first number:- "))
+Num2 = float(input("Enter the second number:- "))
+print("The first number you assigned is Positve." if Num1 > 0  else "The first number you assigned is Negetive.")
+print("The second number you assigned is Positive" if Num2 > 0 else "The second number you assigned is Negetive")
+Value = int(input("Enter the value:- "))
+Result = "The value you assigned is odd number." if Value % 2 == 0 else "The value you assigned is even number."
+print(Result)
+
+
+
+# Simillarly we can practice more examples like this.
