489cdf505b5bfe5bf5dd8f180f9867076c1f7bfa
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-28 20:43:46 +0545

    Modification on 13 and 15 line.

--- Strings_Methods.py
+++ Strings_Methods.py
@@ -3,8 +3,14 @@ STRINGS = input("Enter your string:- ")
 
 Result = len(STRINGS) #Gives the length of the character(i.e number of characters including spaces.)
 print(Result)
-Result = STRINGS.find(" ") # Finding the occurence of the space between any sstrings.
+Result = STRINGS.find("") # Finding the occurence of the space between any sstrings.
 print(Result)
 # While finding the occurences of the characters or any spaces and symbols in string the procedure of finding the occurence always starts from the 0 position of the first character.
 
-Result = STRINGS.capitalize
+Result = STRINGS.capitalize()
+print(Result)
+
+Result = STRINGS.isdigit()
+print(Result)
+Result = STRINGS.isalpha()
+print(Result)
