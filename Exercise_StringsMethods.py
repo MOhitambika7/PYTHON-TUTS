4156aa3bc587c7bf3574ab0313013488877e36e1
MOhitambika7 <mohitambikanand7@gmail.com>
2024-12-29 18:39:13 +0545

    Simple exercise upto strings methods.

---
+++ Exercise_StringsMethods.py
@@ -0,0 +1,17 @@
+# Validate user input exercise.
+# User name no more than 12 character.
+# Username must not contain any spaces.
+# Username must not contain digits.
+
+username = input("Enter yuor username:- ")
+if len(username) > 12:
+    print("Length of the username ie too long. Enter username having characters less tha 12.")
+    
+elif not username.find(" ") == -1:
+    print("Your Username should not contain any spaces.")
+
+elif not username.isalpha():
+    print("Username must not contain any digits.")
+            
+else:
+    print(f"Welcome {username}")    
