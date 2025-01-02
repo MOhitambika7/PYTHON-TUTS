72b2520cf02020a852f5487fe4df32fd21a57abf
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-02 09:49:20 +0545

    This is the code of for_Loop.

---
+++ Loops.py
@@ -0,0 +1,24 @@
+name = "Abhishek"
+for i in name:
+    print(i)
+    if(i == "b"):
+        print("This is something special.")
+colours = ["Red", "Green", "Violet", "Purple", "Grey"]
+for color in colours:
+    print(color)
+    if(color == "Green"):
+        print("This is my favrouite colour.")
+    else:
+        print("This is not my favrouite colour.")  
+        
+for k in range(5):
+    print(k + 1)  
+
+for s in range(1, 9):
+    print(s)              
+
+for x in range(1, 9):
+    print(x + 1)
+
+for a in range(1, 18, 2):
+    print(a)            
