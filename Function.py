a290d395a4c76ee845c00cdaeb637631a2dc5e2a
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-08 22:24:27 +0545

    This is the code for the functions and how it works.

---
+++ Function.py
@@ -0,0 +1,11 @@
+def isGreater(num1, num2):
+    if(num1 > num2):
+        print(f"The number {num1} is greater than {num2}")
+    else:
+        print(f"The number {num2} is greater than {num1}")
+
+
+num1 = int(input("Enter a number num1 = "))
+num2 = int(input("Enter a number num2 = ")) 
+
+isGreater(num1, num2)           
